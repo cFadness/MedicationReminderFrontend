c720c70cf446109594b7204793c264cba7b44b0a
cFadness <codie1989@gmail.com>
2021-12-17 15:19:12 -0800

    Finish setting up the App.jsx file with basic methods and import the NotFoundPage into the App.jsx file.

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from "react";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import NotFoundPage from './NotFound/NotFoundPage';
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            currentUser: null
+        }
+
+    }
+
+    componentDidMount(){
+       this.checkUser()
+    }
+
+    checkUser=()=>{
+        const jwt = localStorage.getItem('token');
+        try{
+            const user = jwtDecode(jwt);
+            this.setState({
+                currentUser: user
+            });
+            
+        }
+        catch(err){
+            console.log("Error decoding token", err);
+            this.setState({
+                currentUser: null
+            });
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <Switch>
+                    <Route path='/' exact render={props => {
+                        if (!this.state.currentUser){
+                            return <Redirect to='/register' />
+                        } else {
+                            return <Redirect to='/yourMedications' />
+                        }
+                    }}
+                    />
+                    <Route path='/notFound' component={NotFoundPage} />
+                    <Redirect to='/notFound' />
+                </Switch>
+            </div>
+        );
+    }
+}
+
+
+export default App;
