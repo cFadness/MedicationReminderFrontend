83fb719d31fac1c4b2086c95428f7cb671967761
cFadness <codie1989@gmail.com>
2021-12-20 10:54:21 -0800

    Setup the YourMedicationsPage as a class component.

---
+++ src/components/YourMedications/YourMedicationsPage.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import YourMedicationsTable from './YourMedicationsTable';
+
+class YourMedicationsPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            arrayOfMedications: []
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+
+    }
+}
+
+
+export default YourMedicationsPage;
