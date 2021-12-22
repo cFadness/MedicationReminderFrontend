324d919975312f9bcea7329771103d921c218aa8
cFadness <codie1989@gmail.com>
2021-12-22 14:37:25 -0800

    Add 'removeMedication' method to the UpdateMedicationPage.

--- src/components/UpdateMedication/UpdateMedicationPage.jsx
+++ src/components/UpdateMedication/UpdateMedicationPage.jsx
@@ -28,7 +28,7 @@ class UpdateMedicationPage extends Component {
     editMedication = async (editedInfo, medId) => {
         try{
             const jwt = localStorage.getItem('token');
-            const response = await axios.post(`http://localhost:5000/api/users/medications/${medId}`, editedInfo, {headers: {'x-auth-token': jwt}})
+            const response = await axios.put(`http://localhost:5000/api/users/medications/${medId}`, editedInfo, {headers: {'x-auth-token': jwt}})
             console.log(response.data)
             this.setState({
                 currentForm: 2
@@ -36,6 +36,20 @@ class UpdateMedicationPage extends Component {
         }
         catch(err){
             console.log("Error editing medication", err)
+        }
+    }
+
+    removeMedication = async (medId) => {
+        try{
+            const jwt = localStorage.getItem('token');
+            const response = await axios.delete(`http://localhost:5000/api/users/medications/${medId}`, {headers: {'x-auth-token': jwt}})
+            console.log(response.data)
+            this.setState({
+                currentForm: 3
+            })
+        }
+        catch(err){
+            console.log("Error deleting medication", err)
         }
     }
 
