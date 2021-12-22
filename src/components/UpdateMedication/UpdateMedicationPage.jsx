2862c56d2c9682b86aa8beec07ab2fe272c63b60
cFadness <codie1989@gmail.com>
2021-12-22 14:11:47 -0800

    Create a class component for the UpdateMedicationPage and add the 'addMedication' method.

---
+++ src/components/UpdateMedication/UpdateMedicationPage.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+
+class UpdateMedicationPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentForm: 1
+        };
+    }
+
+
+    addMedication = async (newMedication) => {
+        try{
+            const jwt = localStorage.getItem('token');
+            const response = await axios.post('http://localhost:5000/api/users/medications', newMedication, {headers: {'x-auth-token': jwt}})
+            console.log(response.data)
+            this.setState({
+                currentForm: 1
+            })
+        }
+        catch(err){
+            console.log("Error adding medication", err)
+        }
+    }
+
+
+    render(){
+        return(
+           <div>
+               
+           </div>
+        )
+    }
+}
+
+
+export default UpdateMedicationPage;
