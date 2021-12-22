c3b073794665e84f9835c2d369c13d6d46d96df2
cFadness <codie1989@gmail.com>
2021-12-22 14:33:45 -0800

    Add 'editMedication' method to UpdateMedicationPage.

--- src/components/UpdateMedication/UpdateMedicationPage.jsx
+++ src/components/UpdateMedication/UpdateMedicationPage.jsx
@@ -25,6 +25,20 @@ class UpdateMedicationPage extends Component {
         }
     }
 
+    editMedication = async (editedInfo, medId) => {
+        try{
+            const jwt = localStorage.getItem('token');
+            const response = await axios.post(`http://localhost:5000/api/users/medications/${medId}`, editedInfo, {headers: {'x-auth-token': jwt}})
+            console.log(response.data)
+            this.setState({
+                currentForm: 2
+            })
+        }
+        catch(err){
+            console.log("Error editing medication", err)
+        }
+    }
+
 
     render(){
         return(
