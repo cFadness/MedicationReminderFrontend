0b18232bf0ad85ecb3a7f73e57643bdacd4fddbb
cFadness <codie1989@gmail.com>
2021-12-27 22:30:51 -0800

    Add comments in the RemoveMedicationForm on what to do next.

---
+++ src/components/UpdateMedication/RemoveMedicationForm.jsx
@@ -0,0 +1,5 @@
+// Add the 'remove medication' form
+// Add alerts to each form if user input is invalid
+// Proceed to style the entire app
+// Add color coded table in regard to quantity remaining and refills remaining
+// Add polish to app as ideas arise
